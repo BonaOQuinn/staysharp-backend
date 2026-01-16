f704bf76c1f5f490e09a97d1a7844eca97d025ba
Bona.OQuinn <bonatoquinn@gmail.com>
2026-01-16 12:57:36 -0800

    Fix seed.sql inserts

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,16 +1,19 @@
 --initial data for demo
-INSERT INTO services (name, duration_minutes, price_cents)
-VALUES ('Haircut', 30, 3000), 
-       ('beard trim', 15, 1500)
-ON CONFLICT DO NOTHING; 
-
-INSERT INTO barbers (name)
-VALUES ('barber 1', 1), 
-       ('barber 2', 1)
-ON CONFLICT DO NOTHING; 
-
-INSERT INTO locations (name, address1, city, state, zip)
+INSERT INTO locations (name, address1, city, state, zip, phone, is_active)
 VALUES
-       ('StaySharp - La Mesa', '5288 Baltimore Drive', 'La Mesa', 'CA', 91942), 
-       ('StaySharp - Spring Valley', '9903 Campo Road', 'Spring Valley', 'CA', 91977); 
+       ('StaySharp - La Mesa', '5288 Baltimore Drive', 'La Mesa', 'CA', 91942, NULL, true), 
+       ('StaySharp - Spring Valley', '9903 Campo Road', 'Spring Valley', 'CA', 91977, NULL, true); 
+
+
+INSERT INTO services (name, duration_minutes, price_cents, is_active)
+VALUES ('Haircut', 30, 3000, true), 
+       ('beard trim', 15, 1500, true);
+
+INSERT INTO barbers (name, location_id, is_active)
+VALUES ('barber 1', 1, true), 
+       ('barber 2', 2, true);
+
+
+
+
 
