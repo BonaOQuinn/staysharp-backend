2a1deddfe5735a4137f0e1429fa244b16168db4d
Bona.OQuinn <bonatoquinn@gmail.com>
2026-01-17 08:33:01 -0800

    Manually inserting ID primary keys for demo

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,17 +1,28 @@
 --initial data for demo
-INSERT INTO locations (name, address1, city, state, zip, phone, is_active)
+INSERT INTO locations (id, name, address1, city, state, zip, phone, is_active)
 VALUES
-       ('StaySharp - La Mesa', '5288 Baltimore Drive', 'La Mesa', 'CA', 91942, NULL, true), 
-       ('StaySharp - Spring Valley', '9903 Campo Road', 'Spring Valley', 'CA', 91977, NULL, true); 
+       (1, 'StaySharp - La Mesa', '5288 Baltimore Drive', 'La Mesa', 'CA', 91942, NULL, true), 
+       (2, 'StaySharp - Spring Valley', '9903 Campo Road', 'Spring Valley', 'CA', 91977, NULL, true); 
 
 
-INSERT INTO services (name, duration_minutes, price_cents, is_active)
-VALUES ('Haircut', 30, 3000, true), 
+INSERT INTO services (id, name, duration_minutes, price_cents, is_active)
+VALUES (1, 'Haircut', 30, 3000, true), 
        ('beard trim', 15, 1500, true);
 
-INSERT INTO barbers (name, location_id, is_active)
-VALUES ('barber 1', 1, true), 
-       ('barber 2', 2, true);
+INSERT INTO barbers (id, name, location_id, is_active)
+VALUES (1, 'barber 1', 1, true), 
+       (2, 'barber 2', 1, true);
+
+
+INSERT INTO working_hours (id,barber_id, dow, start_time, end_time)
+VALUES
+(1, 1, 1, '09:00', '17:00'),
+(2, 1, 2, '09:00', '17:00'), 
+(2, 1, 3, '09:00', '17:00'), 
+(3, 1, 4, '09:00', '17:00'), 
+(4, 1, 5, '09:00', '17:00'), 
+ON CONFLICT (barber_id, dow) DO UPDATE 
+SET start_time = EXCLUDED.start_time, end_time = EXCLUDED.end_time; 
 
 
 
